2b35d8f2d7c4d87237e8f1477b397d0db5854cc4
alvoniah <alvoniah24@gmail.com>
2023-08-17 15:28:13 -0700

    password

--- 0x17-doubly_linked_lists/crackme4.c
+++ 0x17-doubly_linked_lists/crackme4.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
-#!/bin/bash
-pwd = input('Password: ')
-ok = 'Zen of Python'
-ok = ok + ' C '
-	ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1 ]
-print(ok)
-	if (pwd = ok) :
-	print('OK')
-	else:
-	print('KO')
+#include <string.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char pwd[100];
+	char ok[100] = "Zen of Python";
+
+	printf("Password: ");
+	scanf("%s", pwd);
+
+	strcat(ok, " C ");
+	char temp[100];
+
+	strcpy(temp, ok + 1);
+	temp[2] = ok[3];
+	temp[3] = ok[14];
+	temp[4] = ok[3];
+	strncpy(temp + 5, ok + 7, 2);
+	strncpy(temp + 7, ok, 6);
+	temp[13] = '\0';
+
+	printf("%s\n", temp);
+
+	if (strcmp(pwd, temp) == 0)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("KO\n");
+	}
+	return (0);
+}
